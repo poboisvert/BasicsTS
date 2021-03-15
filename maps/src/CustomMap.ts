075dd5b9da7d443f6bac517b4df38581780efcec
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-15 12:57:56 -0400

    Custom infoWindow

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -3,6 +3,7 @@ interface Mappable {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -29,7 +30,7 @@ export class CustomMap {
 
     marker.addListener("click", () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: "Hello",
+        content: mappable.markerContent(),
       });
       infoWindow.open(this.googleMap, marker);
     });
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -8,10 +8,18 @@ export class User {
   };
 
   constructor() {
+    // Usually at the top
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()), // Convert String to Numbers parseFloat
       lng: parseFloat(faker.address.longitude()),
     };
+  }
+  markerContent(): string {
+    return `
+    <h1>
+    User is ${this.name}
+    </h1>
+    `;
   }
 }
